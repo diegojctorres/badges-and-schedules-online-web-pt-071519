963f551b58ecf1637d06b00be1cfc81b43627123
diegojctorres <diegojctorres@gmail.com>
2019-08-02 00:26:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,13 +7,13 @@ end
 
 def batch_badge_creator(speakers)
   speakers.map do |attendee|
-    "Hello, my name is #{attendee}."
+    "Hello, my name is #{speaker}."
   end
 end
 
 def assign_rooms(speakers)
-  speakers.each_with_index.map do |attendee, index|
-    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  speakers.each_with_index.map do |speaker, index|
+    "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
   end
 end
 
